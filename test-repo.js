e008cf874c2b4c6c2a0004ea77f9fbdb5311c16d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-11-02 11:31:03 -0400

    use path.join

--- test-repo.js
+++ test-repo.js
@@ -1,5 +1,6 @@
 const shell = require('shelljs')
 const {existsSync} = require('fs')
+const {join} = require('path')
 
 shell.set('-v') // verbose
 shell.set('-e') // any error is fatal
@@ -22,7 +23,8 @@ shell.cd(repoName)
 shell.exec('git log -1')
 shell.rm('-rf', '.git')
 shell.exec('npm install')
-shell.exec('../node_modules/.bin/commit-message-install')
+const cmi = join('..', 'node_modules', '.bin', 'commit-message-install')
+shell.exec(cmi)
 // show what commands are available
 shell.exec('npm run')
 shell.exec(`npm run ${testCommand}`)
