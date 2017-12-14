0162b7ac3c5c9950b0b665f88c83bd8343d610f9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-14 14:37:58 -0500

    feat: switch to specific branch when testing, close #10

--- test-repo.js
+++ test-repo.js
@@ -1,6 +1,7 @@
 const shell = require('shelljs')
-const {existsSync} = require('fs')
-const {join} = require('path')
+const { existsSync } = require('fs')
+const { join } = require('path')
+const { getJsonFromGit } = require('commit-message-install')
 
 shell.set('-v') // verbose
 shell.set('-e') // any error is fatal
@@ -18,13 +19,37 @@ console.log('using command', testCommand)
 if (existsSync(repoName)) {
   shell.rm('-rf', repoName)
 }
-shell.exec(`git clone --depth 1 ${url}`)
+// need to clone entire repo so we get all branches
+// because we might be testing in a separate branch
+shell.exec(`git clone ${url}`)
 shell.cd(repoName)
-shell.exec('git log -1')
-shell.rm('-rf', '.git')
-shell.exec('npm install')
-const cmi = join('..', 'node_modules', '.bin', 'commit-message-install')
-shell.exec(cmi)
-// show what commands are available
-shell.exec('npm run')
-shell.exec(`npm run ${testCommand}`)
+
+// now see if the commit message tells us a specific branch to test
+getJsonFromGit().then(json => {
+  json = {
+    branch: '1.2.0'
+  }
+  if (json && json.branch) {
+    console.log('commit message specifies branch to test', json.branch)
+    console.log('trying to switch to remote branch', json.branch)
+    const cmd = `git checkout ${json.branch}`
+    try {
+      shell.exec(cmd)
+    } catch (e) {
+      console.error('Caught error trying to do', cmd)
+      console.error(e.message)
+      console.error('assuming we can work in the default branch')
+    }
+  } else {
+    console.log('there is no JSON / version in the commit message')
+  }
+
+  shell.exec('git log -1')
+  shell.rm('-rf', '.git')
+  shell.exec('npm install')
+  const cmi = join('..', 'node_modules', '.bin', 'commit-message-install')
+  shell.exec(cmi)
+  // show what commands are available
+  shell.exec('npm run')
+  shell.exec(`npm run ${testCommand}`)
+})
